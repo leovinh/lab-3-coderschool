adbc2fd40e85f0fdb55cc9d6b36ef8b098eea427
leovinh <thevinhduong@gmail.com>
2016-03-23 19:27:47 +0700

    setup pod

---
+++ lab3coderschool/Pods/Target Support Files/Pods-lab3coderschool/Pods-lab3coderschool-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_lab3coderschool : NSObject
+@end
+@implementation PodsDummy_Pods_lab3coderschool
+@end
---
+++ lab3coderschool/Pods/Target Support Files/Pods-lab3coderschool/Pods-lab3coderschool-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_lab3coderschoolVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_lab3coderschoolVersionString[];
+
